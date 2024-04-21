3ed6d5a8ddd03b1c94579f17d79e13fe175165de
majkelp1982 <s.pomazanka@gmail.com>
2024-04-21 15:46:18 +0200

    update firmware

--- src/main/java/pl/smarthouse/externallightsmodule/properties/Esp32ModuleProperties.java
+++ src/main/java/pl/smarthouse/externallightsmodule/properties/Esp32ModuleProperties.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 public class Esp32ModuleProperties {
 
   // Module specific
-  public static final String FIRMWARE = "20231202.08";
-  public static final String VERSION = "20231202.09";
+  public static final String FIRMWARE = "20240421.15";
+  public static final String VERSION = "20240421.15";
   public static final String MAC_ADDRESS = "24:DC:C3:9F:EE:DC";
   public static final String MODULE_TYPE = "EXTERNAL_LIGHTS";
 }
